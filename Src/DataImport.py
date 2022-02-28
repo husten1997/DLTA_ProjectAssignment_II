fdf3e9458d095924c1854e6701b5e12e8a3b4a6a
Julian Dietlmeier <julian@dietlmeiers.de>
2022-02-28 14:24:39 +0100

    Added a documentation string to each function

--- Src/DataImport.py
+++ Src/DataImport.py
@@ -11,7 +11,12 @@ from os.path import exists
 #
 #   overwrite: Boolean value which determines if the available csv files would be overwritten by new ones
 #              (means DataDownload is used, even though the data is locally available)
-def DataImport(overwrite = False):
+def DataImport(overwrite: bool = False):
+    """
+    Wrapper function for data download and import. It checks if a *.csv file is present and handles the data download and data transformation if it's not.
+    :param overwrite: Boolean value which determines if the available csv files would be overwritten by new ones (means DataDownload is used, even though the data is locally available)
+    :return: DataFrame
+    """
     if not exists("Data/all_data.csv") or overwrite:
         DataDownload()
 
@@ -45,7 +50,34 @@ def DataImport(overwrite = False):
 # for the label stages.
 # This DataFrame is then saved as csv and can be loaded at a later point in time.
 def DataDownload():
-    #%%
+    """
+    Function which covers the data download and transformation. Therefore the raw data is fetched from the server
+    (requires VPN). Afterwards two helper functions are created, one which basically an element-wise application of
+    str.contains to a list. The other helper function contains the logic required for the label selection if more
+    (or less) than 4 labels are available for one text (=Question/Answer). Then a dictionary for the raw_data is created,
+    which will contain a list for "strat"-, "end"-, "text"- and "label"-data. Then a for loop iterates through the raw
+    data and appends the information to the respective lists.
+    This results in a DataFrame which contains an entry for each label, or in other words for each question/answer in
+    each doc (ideally) four entries with ID, and the three question/answer levels.
+    Next step shall be combining the 4 stages of the labels together. This requires a matching of the text and/or
+    start/end information. Because the text and/or start/end information for each label can not be directly matched
+    (because sometimes there are additional characters included at the beginning or end of the text, therefore the stat
+    and end information can also be different) a matrix with the absolute distances is calculated.
+    Afterwards an uncertainty factor is set (to a distance of 10) and a list of all elements which have a distance of less
+    than 10 to each other is created. This ensures that the entries which are the closest to each other are
+    combined/matched. Now the label information of each stage can be combined. For that a for loop iterates through each
+    "label-group" ("label-group" refers to the 3, 4 or 5 labels which are closest to each other) and selects the
+    label-name to one of the for label-stages (QID/AID, label_l1, label_l2, label_l3). If more than 1 label is present
+    for one label stage (for example if a question has two times question_3_* labels) in a first step the algorithm
+    tries to use np.unique to see if the two question_3_* labels are actually different. If thats the case, one can just
+    use one of the two labels without loss of information. If that's NOT the case, one has to choose to drop information
+    (then only the FIRST of the two labels is used) or include the information (then an array of the two labels is used,
+    which obviously breaks the unique labeling if each text).
+    After four label stages are matched to the text, one obtains a DataFrame with one column for text and four columns
+    for the label stages.
+    This DataFrame is then saved as csv and can be loaded at a later point in time.
+    :return: DataFrame
+    """
     LABEL_STUDIO_URL = 'http://132.231.59.226:8080'  # this address needs to be the same as the address the label-studio is hosted on.
     API_KEY = '430d989b7f9723b1e5d82462ded8a22bfb331c6d'  # please add your personal API_Key here to get your API_Key follow the Pictures below
 
@@ -54,8 +86,13 @@ def DataDownload():
     pro = project.Project.get_from_id(ls, "1")
     tasks = project.Project.get_labeled_tasks(pro)
 
-    # %%
-    def arrayContains(x, y):
+    def arrayContains(x: list, y:str):
+        """
+        Checks of an (str) element of a list contains y
+        :param x: List or iterable where each element is a str
+        :param y: str
+        :return: List of bools
+        """
         output = []
         for a in x:
             output.append(str(a).__contains__(y))
@@ -65,6 +102,7 @@ def DataDownload():
     strange_list = pd.DataFrame(columns=['docID', 'start', 'end', 'text', 'label', 'id'])
 
     def selectLabel(array, label, first=True):
+        """Algorithm which selects a label"""
         nonlocal strange_list
         label_array = list(array.loc[arrayContains(array['label'], label), 'label'])
         if len(label_array) == 1:
--- Src/DataPreprocessing.py
+++ Src/DataPreprocessing.py
@@ -19,7 +19,14 @@ from nltk.stem import WordNetLemmatizer
 #               the index is replaces by a new index. If the old index is import it can be saved as column of
 #               the DataFrame
 
-def dataCheck(data, keepNA = False, keepOldIDs = False):
+def dataCheck(data: pd.DataFrame, keepNA: str = False, keepOldIDs: str = False):
+    """
+    Checks the data for missing values (na) and (optionally) removes them.
+    :param data: data object (pd.DataFrame)
+    :param keepNA: the rows containing nas are not dropped, the function then only returns the na count per column and number or rows containing at least one na
+    :param keepOldIDs: the pd.dropna function drops the rows containing nas, but does not change the indexing, which leads to missing index values. Therefore, for loops iterating over row-indices will not work anymore. Therefore, the index is replaces by a new index. If the old index is import it can be saved as column of the DataFrame
+    :return: DataFrame
+    """
     print(data.isna().sum())
 
     rowwiseIsNA = np.vectorize(pd.isna)
@@ -49,7 +56,19 @@ def dataCheck(data, keepNA = False, keepOldIDs = False):
 #
 #   keepOriginalData: Boolean-Value which determines if the original text data is saved or not
 #
-def textPreprocessing(data, input_col = 'text', keepSteps = True, keepOriginalData = True):
+def textPreprocessing(data: pd.DataFrame, input_col: str = 'text', keepSteps: bool = True, keepOriginalData: bool = True) -> pd.DataFrame:
+    """
+    Wrapper function for the text preprocessing. Applies the following text preprocessing steps:
+        1) removes capitalisation (output column of this step: 'proText_C')
+        2) removes punctuation (output column of this step: 'proText_CP')
+        3) removes stopwords (output column of this step: 'proText_CPS')
+        4) Lemmitazation (stemming) of the tokens (output column of this step: 'proText_CPSL')
+    :param data: data object (pd.DataFrame)
+    :param input_col: name of the column containing the original text
+    :param keepSteps: Boolean-Value which determines if the columns which results after each of the four steps are dropped or kept
+    :param keepOriginalData: Boolean-Value which determines if the original text data is saved or not
+    :return: Data Frame
+    """
     data = textCapitalisation(data)
     data = textRmPunctuaion(data)
     data = textRmStopwords(data)
@@ -79,8 +98,17 @@ def textPreprocessing(data, input_col = 'text', keepSteps = True, keepOriginalDa
 #   output_col: Name of the column which will contain the texts after the application of the current
 #               step/transformation.
 #
-def textCapitalisation(data, input_col = 'text', output_col ='proText_C'):
-    # %% Data Preparation - Capitalisation
+def textCapitalisation(data: pd.DataFrame, input_col: str = 'text', output_col: str ='proText_C') -> pd.DataFrame:
+    """
+    Function which covers the removal of the capitalisation. Therefore, the np.vectorize function is used to create a new
+    function which returns an array of strings. Each element in this array of strings is created from one element of the
+    text column to which the str.lower function is applied. This procedure basically leads to an elementwise application
+    of the str.lower function. The results are then saved in a new column.
+    :param data: Data object (pd.DataFrame)
+    :param input_col: Name of the column containing the strings of the previous step.
+    :param output_col: Name of the column which will contain the texts after the application of the current step/transformation.
+    :return: DataFrame which includes a text colum where the punctuation was removed
+    """
     rmCap = np.vectorize(str.lower)
     data[output_col] = rmCap(data[input_col])
 
@@ -100,8 +128,18 @@ def textCapitalisation(data, input_col = 'text', output_col ='proText_C'):
 #   output_col: Name of the column which will contain the texts after the application of the current
 #               step/transformation.
 #
-def textRmPunctuaion(data, input_col = 'proText_C', output_col ='proText_CP'):
-    # %% Data Preparation - Remove leading/trailing spaces and punctuation
+def textRmPunctuaion(data: pd.DataFrame, input_col: str = 'proText_C', output_col: str = 'proText_CP') -> pd.DataFrame:
+    """
+    Function which covers the removal of punctuation. Therefore, the punctuation, digits from the strings library and some
+    costume strings are used. With the help of the maketrans and translate function each punctuation and digit character
+    is removed (or rather replaced by '') in each text. Additionally, double spaces are replaced, which could result from
+    the removal of punctuation (e.x. "We spend 100$ on xy." => "We spend  on xy"). Afterwards leading and trailing
+    spaces are removed from the texts.
+    :param data: Data object (pd.DataFrame)
+    :param input_col: Name of the column containing the strings of the previous step
+    :param output_col: Name of the column which will contain the texts after the application of the current step/transformation.
+    :return: DataFrame which includes a text colum where the punctuation was removed
+    """
     for s in range(data.shape[0]):
         data.loc[s, output_col] = data.loc[s, input_col].translate(
             str.maketrans('', '', string.punctuation + string.digits + "–" + "‘"))  # .replace("'", "")
@@ -128,8 +166,20 @@ def textRmPunctuaion(data, input_col = 'proText_C', output_col ='proText_CP'):
 #   output_col: Name of the column which will contain the texts after the application of the current
 #               step/transformation.
 #
-def textRmStopwords(data, input_col ='proText_CP', output_col ='proText_CPS'):
-    # %% Data Preparation - Removal of stopwords
+def textRmStopwords(data: pd.DataFrame, input_col ='proText_CP', output_col ='proText_CPS'):
+    """
+    Function which covers the removal of stopwords. For that the nltk packages is used which contains a list
+    of common (english) stopwords. To this list were some obviously missing words added. Because of the previous step,
+    the stopwords have to be stripped of their punctuation. Then each string element is seperated into its tokens and all
+    tokens which are in the list of stopwords are removed. Afterwards a new string is created. The results have shown
+    that some special characters where not removed correctly, therefore the punctuation is removed again.
+    After that the resulting word list and word frequency's in the corpus where analysed (upper 5% of word frequencies)
+    and a list of additional stopwords (with high frequencies) was created, which are then removed separately.
+    :param data: Data object (pd.DataFrame)
+    :param input_col: Name of the column containing the strings of the previous step.
+    :param output_col: Name of the column which will contain the texts after the application of the current step/transformation.
+    :return: DataFrame which includes a text colum where the stopwords were removed
+     """
     nltk.download('stopwords')
     nltk.download('punkt')
 
@@ -173,10 +223,16 @@ def textRmStopwords(data, input_col ='proText_CP', output_col ='proText_CPS'):
 #               step/transformation.
 #
 def textLemmatization(data, input_col ='proText_CPS', output_col ='proText_CPSL'):
-    # %% Data Preparation - Lemmatization
+    """
+    Function which covers lemmatization. Again the nltk packages is used here. Each element in the text columns is again
+    seperated into its tokens and then the stem of the corresponding token is returned and combined back to a string.
+    :param data: Data object (pd.DataFrame)
+    :param input_col: Name of the column containing the strings of the previous step.
+    :param output_col: Name of the column which will contain the texts after the application of the current step/transformation.
+    :return: DataFrame which includes a text colum where the stemming was done
+    """
     nltk.download('wordnet')
     nltk.download('omw-1.4')
-
 
     tk = SpaceTokenizer()
     lemmatizer = WordNetLemmatizer()
