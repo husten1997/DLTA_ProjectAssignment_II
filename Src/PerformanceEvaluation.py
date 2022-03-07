a7dc04439cef3081120bdb07e9803982c6c435a0
Fabi9610 <94356456+Fabi9610@users.noreply.github.com>
2022-03-07 17:19:20 +0100

    Update PerformanceEvaluation.py

--- Src/PerformanceEvaluation.py
+++ Src/PerformanceEvaluation.py
@@ -2,7 +2,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve, plot_roc_curve
+from sklearn.metrics import roc_curve, plot_roc_curve, roc_auc_score
+import matplotlib.pylab as plt
 
 
 def calculateConfusionMatrix(data_true: pd.Series, data_pred: pd.Series, freq: bool = True):
@@ -28,25 +29,21 @@ def plotROC(data_true: pd.Series, data_pred: pd.Series):
 
 
 
-y_coi = pd.DataFrame(data = y_coi)
-y_coi.dropna(inplace = True)
-y_coi = y_coi[0:100]
+def roc_measure(data,doc_type, label_stage, data_pred):
 
-data_pred = np.random.uniform(0,1,len(y_coi))
-data_pred = list(data_pred)
+    '''
 
-
-
-def roc_measure(class_dimension, doc_type, label_stage, data_pred, threshold):
+    :param data: data with labeled text
+    :param doc_type: Question or answer
+    :param label_stage: stage of the classified labels
+    :param data_pred: predictions of a classifier with probabilities as output
+    :return: roc table, roc curve and area under the curve per probability
+    '''
 
 
     data_true = []
-    roc_table = pd.DataFrame(columns = ["threshold", "TPR", "FPR"], index = [i for i in range(100)])
+    roc_table = pd.DataFrame(columns = ["threshold", "TPR", "FPR", "AUC"], index = [i for i in range(100)])
 
-    class_dimension = 1
-
-    doc_type = 'Answer'
-    label_stage = 'label_l1'
 
     for doc in range(data.shape[0]):
         #check if doc type is equal to the category of interest (coi)
@@ -63,37 +60,36 @@ def roc_measure(class_dimension, doc_type, label_stage, data_pred, threshold):
     y_coi = []
 
     for label in range(len(data_true)):
-        y = int(data_true[label] == 'Answer_1_specific')
+        y = int(data_true[label] == 'Question_1_Company_specific')
         y_coi.append(y)
 
-    y_hat = []
+    #binarize predicted data with probability threshold
 
-    for label in range(len(data_true)):
-        y = int(data_pred[label] >= threshold)
-        y_hat.append(y)
 
+    for threshold in range(100):
+
+        y_hat = []
+
+        for label in range(len(data_true)):
+            y = int(data_pred[label] >= threshold/100)
+            y_hat.append(y)
 
         fpr, tpr, _ = roc_curve(y_true = y_coi, y_score = y_hat)
 
-    roc_table.loc[0] = threshold, fpr[1], tpr[1]
+        roc_table.loc[threshold] = threshold/100, fpr[1], tpr[1], roc_auc_score(y_true = y_coi, y_score = y_hat)
+
+    plt.figure()
+    lw = 2
+    plt.plot(
+        roc_table["FPR"],
+        roc_table["TPR"],
+        lw = lw
+    )
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+
+    return roc_table
 
 
 
-plt.figure()
-lw = 2
-plt.plot(
-    fpr[2],
-    tpr[2],
-    color="darkorange",
-    lw=lw,
-    label="ROC curve (area = %0.2f)" % roc_auc[2],
-)
-plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel("False Positive Rate")
-plt.ylabel("True Positive Rate")
-plt.title("Receiver operating characteristic example")
-plt.legend(loc="lower right")
-plt.show()
 
