58675f9daf7d0445e6c1c92039d50d1d6f952a9f
Julian Dietlmeier <julian@dietlmeiers.de>
2022-03-09 17:52:36 +0100

    Update to main

--- Src/main.py
+++ Src/main.py
@@ -19,6 +19,8 @@ from Src.DataPreprocessing import  oversampling
 from Src.PerformanceEvaluation import calculateConfusionMatrix, roc_measure
 from Src.ClassifierLinear import classifierLinear
 from Src.ClassifierNN import classifierNN
+from tqdm import tqdm
+from Src.prompt import prompt
 
 #%% Data Import
 data = DataImport()
@@ -42,15 +44,14 @@ mat_tfidf, tfidf_doc = tf_idf(corpus)
 lsa_doc = LSA(corpus, mat_tfidf)
 
 #%% GloVe
-context_matrix, target_matrix = GloVe(corpus, epochs = 40, eta=0.0001)
+context_matrix, target_matrix = GloVe(corpus, epochs = 40, eta=0.0001, overwrite=False)
 glove_doc = docPresentation_alt(corpus = corpus, embedding_matrix = context_matrix + target_matrix)
 
 #%% word2vec
 w2v_embedding = w2v_matrix(corpus = corpus, window_size = 5,min_count = 1, sg = 1,vector_size = 300)
-
-#%% doc presentation of w2v
 w2v_doc = docPresentation_alt(corpus = corpus, embedding_matrix = w2v_embedding)
 
+"""
 #%% Classifier
 
 # Data Prep
@@ -95,3 +96,89 @@ word_embedding = w2v_doc
 labels = data['label_l1']
 
 vectors, labels = oversampling(word_embedding, labels, 'SMOTE')
+"""
+
+
+#%% Prep Classifier
+
+# Data Prep
+sample_method = "SMOTE"
+nfolds = 10
+encoding_matrix = generateEncodingMatrix(data['label_l1'])
+
+tfidf_sample = dataSample(tfidf_doc, data, method = sample_method, n = 0, folds=nfolds)
+lsa_sample = dataSample(lsa_doc, data, method = sample_method, n = 0, folds=nfolds)
+glove_sample = dataSample(glove_doc, data, method = sample_method, n = 0, folds=nfolds)
+w2v_sample = dataSample(w2v_doc, data, method = sample_method, n = 0, folds=nfolds)
+
+# Sample Split:
+convert_to_dic = lambda x: {l:i for l, i in zip(['dataTrain', 'dataTest'], x)}
+
+tfidf_data = convert_to_dic(dataSplit(tfidf_sample))
+lsa_data = convert_to_dic(dataSplit(lsa_sample))
+glove_data = convert_to_dic(dataSplit(glove_sample))
+w2v_data = convert_to_dic(dataSplit(w2v_sample))
+
+#%% NN Classifier
+# Fitt classifier
+data_tfidf_nn = classifierNN(tfidf_data['dataTrain'], tfidf_data['dataTest'], tfidf_doc, data, encoding_matrix, project_name="NN_tfidf", plot_title="TfIdf - NN Classifier Loss")
+data_lsa_nn = classifierNN(lsa_data['dataTrain'], lsa_data['dataTest'], lsa_doc, data, encoding_matrix, project_name="NN_lsa", plot_title="LSA - NN Classifier Loss")
+data_glove_nn = classifierNN(glove_data['dataTrain'], glove_data['dataTest'], glove_doc, data, encoding_matrix, project_name="NN_glove", plot_title="GloVe - NN Classifier Loss")
+data_w2v_nn = classifierNN(w2v_data['dataTrain'], w2v_data['dataTest'], w2v_doc, data, encoding_matrix, project_name="NN_w2v", plot_title="W2V - NN Classifier Loss")
+
+# Performance Evaluation
+conf_matrix_tfidf = calculateConfusionMatrix(data_tfidf_nn['label_l1'], data_tfidf_nn['label_l1_NNpred'])
+auc_tfidf = roc_measure(data_tfidf_nn, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_NNprob', plot_title = 'TfIdf - ROC Curve')
+
+conf_matrix_lsa = calculateConfusionMatrix(data_lsa_nn['label_l1'], data_lsa_nn['label_l1_NNpred'])
+auc_lsa = roc_measure(data_lsa_nn, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_NNprob', plot_title = 'LSA - ROC Curve')
+
+conf_matrix_glove = calculateConfusionMatrix(data_glove_nn['label_l1'], data_glove_nn['label_l1_NNpred'])
+auc_glove = roc_measure(data_glove_nn, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_NNprob', plot_title = 'GloVe - ROC Curve')
+
+conf_matrix_w2v = calculateConfusionMatrix(data_w2v_nn['label_l1'], data_w2v_nn['label_l1_NNpred'])
+auc_w2v = roc_measure(data_w2v_nn, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_NNprob', plot_title = 'Word to Vec - ROC Curve')
+
+#%% Linear Classifier
+
+#%% Crossvalidation
+resultMatrix = pd.DataFrame(0, columns=['TfIdf', 'LSA', 'GloVe', 'Word to Vec'], index = ['Linear-Classifier', 'Neural-Network-Classifier'])
+prompt("Starting Cross-Validation")
+for i in tqdm(range(1, nfolds+1)):
+    # Sample Split:
+    tfidf_data = convert_to_dic(dataSplit(tfidf_sample, test_fold=i))
+    lsa_data = convert_to_dic(dataSplit(lsa_sample, test_fold=i))
+    glove_data = convert_to_dic(dataSplit(glove_sample, test_fold=i))
+    w2v_data = convert_to_dic(dataSplit(w2v_sample, test_fold=i))
+
+    # Linear Classifier
+    # Fitt classifier
+
+    # Performance Eval
+
+
+
+    # NN Classifier
+    # Fitt classifier
+    data_tfidf_nn = classifierNN(tfidf_data['dataTrain'], tfidf_data['dataTest'], tfidf_doc, data, encoding_matrix,
+                                 project_name="NN_tfidf", plot_title="TfIdf - NN Classifier Loss")
+    data_lsa_nn = classifierNN(lsa_data['dataTrain'], lsa_data['dataTest'], lsa_doc, data, encoding_matrix,
+                               project_name="NN_lsa", plot_title="LSA - NN Classifier Loss")
+    data_glove_nn = classifierNN(glove_data['dataTrain'], glove_data['dataTest'], glove_doc, data, encoding_matrix,
+                                 project_name="NN_glove", plot_title="GloVe - NN Classifier Loss")
+    data_w2v_nn = classifierNN(w2v_data['dataTrain'], w2v_data['dataTest'], w2v_doc, data, encoding_matrix,
+                               project_name="NN_w2v", plot_title="W2V - NN Classifier Loss")
+
+    # Performance Eval
+    resultMatrix.loc['Neural-Network-Classifier', 'TfIdf'] += roc_measure(data_tfidf_nn, encoding_matrix, label_true='label_l1', label_prob='label_l1_NNprob',
+                            plot_title='TfIdf - ROC Curve', plot=False)
+    resultMatrix.loc['Neural-Network-Classifier', 'LSA'] += roc_measure(data_lsa_nn, encoding_matrix, label_true='label_l1', label_prob='label_l1_NNprob',
+                          plot_title='LSA - ROC Curve', plot=False)
+    resultMatrix.loc['Neural-Network-Classifier', 'GloVe'] += roc_measure(data_glove_nn, encoding_matrix, label_true='label_l1', label_prob='label_l1_NNprob',
+                            plot_title='GloVe - ROC Curve', plot=False)
+    resultMatrix.loc['Neural-Network-Classifier', 'Word to Vec'] += roc_measure(data_w2v_nn, encoding_matrix, label_true='label_l1', label_prob='label_l1_NNprob',
+                          plot_title='Word to Vec - ROC Curve', plot=False)
+
+resultMatrix = resultMatrix / nfolds
+
+print(resultMatrix)
