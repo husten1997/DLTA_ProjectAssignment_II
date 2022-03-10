d6d04f476193dec69032a19ac8640b3d4e50bead
nietz01 <nietz01@ads.uni-passau.de>
2022-03-10 13:42:14 +0100

    linear classifier in main implemented after the logic has been rebuilt

--- Src/ClassifierLinear.py
+++ Src/ClassifierLinear.py
@@ -2,19 +2,27 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-def classifierLinear(docRepTrain: pd.DataFrame, docRepresentation: pd.DataFrame, dataTrain: pd.DataFrame,
-                     data: pd.DataFrame, label_col: str ='label_l1', prediction_suffix: str = "_LINpred"):
+def classifierLinear(sampleTrain: pd.DataFrame, docRepresentation: pd.DataFrame, data: pd.DataFrame,
+                     label_col: str ='label_l1', prediction_suffix: str = "_LINpred", prob_suffix: str = "_LINprob"):
+
+    docRepTrain = sampleTrain['doc']
+    docRepTrain = np.matrix([np.array(i) for i in docRepTrain])
+
 
     X_train = np.matrix(docRepTrain)
     X_test = np.matrix(docRepresentation)
 
-    y_train = dataTrain[label_col]
+    y_train = sampleTrain['labels']
 
     logit = LogisticRegression()
     logit.fit(X_train, y_train)
 
     pred_data = logit.predict(X_test)
+    pred_data_prob = logit.predict_proba(X_test)
+
+    pred_data_prob_marketrel = np.array(pred_data_prob)[:, 1]
 
     data[str(label_col + prediction_suffix)] = pred_data
+    data[str(label_col + prob_suffix)] = pred_data_prob_marketrel
 
     return data
--- Src/main.py
+++ Src/main.py
@@ -15,7 +15,7 @@ from Src.W2VWordEmbedding import w2v_matrix
 from Src.W2VDocPresentation import docPresentation
 from Src.DocPresentation import docPresentation_alt
 from Src.DataPreprocessing import dataSample, dataSplit, generateEncodingMatrix
-from Src.DataPreprocessing import  oversampling
+from Src.DataPreprocessing import oversampling
 from Src.PerformanceEvaluation import calculateConfusionMatrix, roc_measure
 from Src.ClassifierLinear import classifierLinear
 from Src.ClassifierNN import classifierNN
@@ -37,7 +37,7 @@ data = dataSelection(data, label_stage=1, doc_type='question')
 #%%
 corpus = data['text']
 
-#%% td-idf
+#%% tf-idf
 mat_tfidf, tfidf_doc = tf_idf(corpus)
 
 #%% LSA
@@ -50,54 +50,6 @@ glove_doc = docPresentation_alt(corpus = corpus, embedding_matrix = context_matr
 #%% word2vec
 w2v_embedding = w2v_matrix(corpus = corpus, window_size = 5,min_count = 1, sg = 1,vector_size = 300)
 w2v_doc = docPresentation_alt(corpus = corpus, embedding_matrix = w2v_embedding)
-
-"""
-#%% Classifier
-
-# Data Prep
-selection_index = dataSample(data, method = "undersample", n = 0)
-encoding_matrix = generateEncodingMatrix(data['label_l1'])
-
-dataTrain, dataTest = dataSplit(data, selection_index)
-
-docTrain_tfidf, docTest_tfidf = dataSplit(tfidf_doc, selection_index)
-docTrain_lsa, docTest_lsa = dataSplit(lsa_doc, selection_index)
-docTrain_glove, docTest_glove = dataSplit(glove_doc, selection_index)
-docTrain_w2v, docTest_w2v = dataSplit(w2v_doc, selection_index)
-
-
-# Classifier Fit
-# Linear Classifier
-test_data_tfidf_lin = classifierLinear(docTrain_tfidf, tfidf_doc, dataTrain, data)
-test_data_lsa_lin = classifierLinear(docTrain_lsa, lsa_doc, dataTrain, data)
-# NN Classifier
-test_data_glove_nn = classifierNN(docTrain_glove, docTest_glove, glove_doc.loc[selection_index, :], dataTrain, dataTest, data.loc[selection_index, :], encoding_matrix)
-test_data_w2v_nn = classifierNN(docTrain_w2v, docTest_w2v, w2v_doc, dataTrain, dataTest, data, encoding_matrix)
-
-#TODO: AN: NN classifier for tdidf and lsa
-
-# Performance Evaluation
-# Linear Classifier
-conf_matrix_tfidf_lin = calculateConfusionMatrix(test_data_tfidf_lin['label_l1'], test_data_tfidf_lin['label_l1_LINpred'])
-conf_matrix_lsa_lin = calculateConfusionMatrix(test_data_lsa_lin['label_l1'], test_data_lsa_lin['label_l1_LINpred'])
-
-# NN Classifier
-conf_matrix_glove = calculateConfusionMatrix(test_data_glove_nn['label_l1'], test_data_glove_nn['label_l1_NNpred'])
-#TODO: AN: confusion matrix for tdidf and lsa
-
-
-#roc test
-NN_w2v_pred = test_data_w2v_nn['label_l1_NNprob']
-roc_measure(data, NN_w2v_pred, encoding_matrix,True,'w2v')
-
-#oversampling example
-
-word_embedding = w2v_doc
-labels = data['label_l1']
-
-vectors, labels = oversampling(word_embedding, labels, 'SMOTE')
-"""
-
 
 #%% Prep Classifier
 
@@ -120,7 +72,7 @@ glove_data = convert_to_dic(dataSplit(glove_sample))
 w2v_data = convert_to_dic(dataSplit(w2v_sample))
 
 #%% NN Classifier
-# Fitt classifier
+# Fit classifier
 data_tfidf_nn = classifierNN(tfidf_data['dataTrain'], tfidf_data['dataTest'], tfidf_doc, data, encoding_matrix, project_name="NN_tfidf", plot_title="TfIdf - NN Classifier Loss")
 data_lsa_nn = classifierNN(lsa_data['dataTrain'], lsa_data['dataTest'], lsa_doc, data, encoding_matrix, project_name="NN_lsa", plot_title="LSA - NN Classifier Loss")
 data_glove_nn = classifierNN(glove_data['dataTrain'], glove_data['dataTest'], glove_doc, data, encoding_matrix, project_name="NN_glove", plot_title="GloVe - NN Classifier Loss")
@@ -140,6 +92,24 @@ conf_matrix_w2v = calculateConfusionMatrix(data_w2v_nn['label_l1'], data_w2v_nn[
 auc_w2v = roc_measure(data_w2v_nn, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_NNprob', plot_title = 'Word to Vec - ROC Curve')
 
 #%% Linear Classifier
+# Fit classifier
+data_tfidf_lin = classifierLinear(tfidf_data['dataTrain'], tfidf_doc, data)
+data_lsa_lin = classifierLinear(lsa_data['dataTrain'], lsa_doc, data)
+data_glove_lin = classifierLinear(glove_data['dataTrain'], glove_doc, data)
+data_w2v_lin = classifierLinear(w2v_data['dataTrain'], w2v_doc, data)
+
+# Performance Evaluation
+conf_matrix_tfidf = calculateConfusionMatrix(data_tfidf_lin['label_l1'], data_tfidf_lin['label_l1_LINpred'])
+auc_tfidf = roc_measure(data_tfidf_lin, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_LINprob', plot_title = 'TfIdf - ROC Curve')
+
+conf_matrix_lsa = calculateConfusionMatrix(data_lsa_lin['label_l1'], data_lsa_lin['label_l1_LINpred'])
+auc_lsa = roc_measure(data_lsa_lin, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_LINprob', plot_title = 'LSA - ROC Curve')
+
+conf_matrix_glove = calculateConfusionMatrix(data_glove_lin['label_l1'], data_glove_lin['label_l1_LINpred'])
+auc_glove = roc_measure(data_glove_lin, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_LINprob', plot_title = 'GloVe - ROC Curve')
+
+conf_matrix_w2v = calculateConfusionMatrix(data_w2v_lin['label_l1'], data_w2v_lin['label_l1_LINpred'])
+auc_w2v = roc_measure(data_w2v_lin, encoding_matrix, label_true = 'label_l1', label_prob = 'label_l1_LINprob', plot_title = 'Word to Vec - ROC Curve')
 
 #%% Crossvalidation
 resultMatrix = pd.DataFrame(0, columns=['TfIdf', 'LSA', 'GloVe', 'Word to Vec'], index = ['Linear-Classifier', 'Neural-Network-Classifier'])
@@ -152,14 +122,24 @@ for i in tqdm(range(1, nfolds+1)):
     w2v_data = convert_to_dic(dataSplit(w2v_sample, test_fold=i))
 
     # Linear Classifier
-    # Fitt classifier
+    # Fit classifier
+    data_tfidf_lin = classifierLinear(tfidf_data['dataTrain'], tfidf_doc, data)
+    data_lsa_lin = classifierLinear(lsa_data['dataTrain'], lsa_doc, data)
+    data_glove_lin = classifierLinear(glove_data['dataTrain'], glove_doc, data)
+    data_w2v_lin = classifierLinear(w2v_data['dataTrain'], w2v_doc, data)
 
     # Performance Eval
-
-
+    resultMatrix.loc['Neural-Network-Classifier', 'TfIdf'] += roc_measure(data_tfidf_lin, encoding_matrix, label_true='label_l1', label_prob='label_l1_LINprob',
+                            plot_title='TfIdf - ROC Curve', plot=False)
+    resultMatrix.loc['Neural-Network-Classifier', 'LSA'] += roc_measure(data_lsa_lin, encoding_matrix, label_true='label_l1', label_prob='label_l1_LINprob',
+                          plot_title='LSA - ROC Curve', plot=False)
+    resultMatrix.loc['Neural-Network-Classifier', 'GloVe'] += roc_measure(data_glove_lin, encoding_matrix, label_true='label_l1', label_prob='label_l1_LINprob',
+                            plot_title='GloVe - ROC Curve', plot=False)
+    resultMatrix.loc['Neural-Network-Classifier', 'Word to Vec'] += roc_measure(data_w2v_lin, encoding_matrix, label_true='label_l1', label_prob='label_l1_LINprob',
+                          plot_title='Word to Vec - ROC Curve', plot=False)
 
     # NN Classifier
-    # Fitt classifier
+    # Fit classifier
     data_tfidf_nn = classifierNN(tfidf_data['dataTrain'], tfidf_data['dataTest'], tfidf_doc, data, encoding_matrix,
                                  project_name="NN_tfidf", plot_title="TfIdf - NN Classifier Loss")
     data_lsa_nn = classifierNN(lsa_data['dataTrain'], lsa_data['dataTest'], lsa_doc, data, encoding_matrix,
